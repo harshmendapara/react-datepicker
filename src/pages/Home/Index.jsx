1f6e93514942f74ac81c21c8bd50704cf6d1b57b
Harsh Mendapara <mendaparaharsh02@gmail.com>
2022-04-15 12:49:19 +0530

    cba nbges

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,47 +1,67 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-import DatePicker, { registerLocale } from "react-datepicker";
+// import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-import ja from "date-fns/locale/ja"; // the locale you want
-registerLocale("ja", ja); // register it with the name you want
+// import ja from "date-fns/locale/ja"; // the locale you want
+// registerLocale("ja", ja); // register it with the name you want
 
-const Home = () => {
-    const [startDate, setStartDate] = useState();
-    const onChangeDate = (date) => {
-        setStartDate(date);
-    };
-    return (
-        <DatePicker
-            locale="ja"
-            dateFormat="yyyy/MM/dd"
-            selected={startDate}
-            onChange={onChangeDate}
-        />
-    );
-};
-
-export default Home;
-
-// import React, { useState } from "react";
-// import DatePicker from "react-datepicker";
 // const Home = () => {
-//     const [newvalue, setValue] = useState(null);
-//     const handleChangeRaw = (value) => {
-//         const replaceValue = value.replace(/ /g, "");
-//         // 0 Index is Years, 1 Index is Months, 2 Index is Days
-//         const spiltValue = replaceValue.split("/");
-//         console.log(spiltValue);
+//     const [startDate, setStartDate] = useState();
+//     const onChangeDate = (date) => {
+//         setStartDate(date);
 //     };
 //     return (
 //         <DatePicker
+//             locale="ja"
 //             dateFormat="yyyy/MM/dd"
-//             onChange={(date, event) =>
-//                 console.log("event.target.value", event.target.value, date)
-//             }
-//             onChangeRaw={(event) => handleChangeRaw(event.target.value)}
+//             selected={startDate}
+//             onChange={onChangeDate}
 //         />
 //     );
 // };
 
 // export default Home;
+
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+const Home = () => {
+    const [startDate, setStartDate] = useState(null);
+    const [newvalue, setValue] = useState(null);
+    const handleChangeRaw = (value) => {
+        console.log(typeof value);
+        if (typeof value == "string") {
+            const replaceValue = value.replace(/ /g, "");
+            // 0 Index is Years, 1 Index is Months, 2 Index is Days
+            const splitData = replaceValue.split("/");
+            setValue(replaceValue);
+            var theBigDay = new Date();
+            if (splitData && splitData.length > 0 && splitData[0]) {
+                theBigDay.setYear(splitData[0]);
+                setStartDate(theBigDay);
+            }
+            if (splitData && splitData.length > 1 && splitData[1]) {
+                theBigDay.setMonth(splitData[1] - 1);
+                setStartDate(theBigDay);
+            }
+            if (splitData && splitData.length > 2 && splitData[2]) {
+                theBigDay.setDate(splitData[2]);
+                setStartDate(theBigDay);
+            }
+        } else {
+            setStartDate(value);
+        }
+    };
+    return (
+        <DatePicker
+            selected={startDate}
+            dateFormat="yyyy/MM/dd"
+            onChange={(date, event) => {
+                setStartDate(date);
+            }}
+            onChangeRaw={(event) => handleChangeRaw(event.target.value)}
+        />
+    );
+};
+
+export default Home;
